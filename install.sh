c6edf362c681190b3370d5c6a1f8cd6c9609ba88
Daniel Arndt <dan@arndt.ca>
2014-12-25 04:40:02 +0000

    Modified install script to configure more. * Install zsh * Remind user to set up a different editor * Clone the antigen repo, a zsh plugin manager * Link the zshrc * Change shell to zsh and launch it

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
-DA_CONFIG_LOCATION="$HOME/config"
+#!/bin/bash
 
-sudo apt-get install zsh
-ln -s $DA_CONFIG_LOCATION/zsh/.zshrc ~/.zshrc
+# Install zsh
+sudo apt-get install git zsh
+
+# Remind user to set up a different editor
+sudo update-alternatives --config editor
+
+# Clone the antigen repo, a zsh plugin manager
+git clone https://github.com/zsh-users/antigen.git ~/config/zsh/antigen
+
+# Link the zshrc
+ln -i -s ~/config/zsh/.zshrc ~/.zshrc
+
+# Change shell to zsh and launch it
+chsh -s /bin/zsh
+if [ $SHELL != "/bin/zsh" ]; then
+    echo "Switching to zsh"
+    zsh
+fi
