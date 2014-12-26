c75f49f98438955d965667a3cbc0e63d566f1ca7
Daniel Arndt <dan@arndt.ca>
2014-12-26 18:12:18 +0000

    Modularized installation script.

--- install.sh
+++ install.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-# Install zsh
-sudo apt-get install git zsh
+ask() {
+    while true; do
+ 
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+ 
+        # Ask the question
+        read -p "$1 [$prompt] " REPLY
+ 
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+ 
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+ 
+    done
+}
 
 # Remind user to set up a different editor
-sudo update-alternatives --config editor
+if ask "Would you like to switch default editors?"; then
+    sudo update-alternatives --config editor
+fi
 
-# Clone the antigen repo, a zsh plugin manager
-git clone https://github.com/zsh-users/antigen.git ~/config/zsh/antigen
-
-# Link the zshrc
-ln -i -s ~/config/zsh/.zshrc ~/.zshrc
-
-# Change shell to zsh and launch it
-chsh -s /bin/zsh
-if [ $SHELL != "/bin/zsh" ]; then
-    echo "Switching to zsh"
-    zsh
+if ask "Would you like to install zsh?"; then
+    exec ~/config/installZsh.sh
 fi
---
+++ installZsh.sh
@@ -0,0 +1,16 @@
+# Install zsh
+sudo apt-get install git zsh
+
+# Clone the antigen repo, a zsh plugin manager
+git clone https://github.com/zsh-users/antigen.git ~/config/zsh/antigen
+
+# Link the zshrc
+ln -i -s ~/config/zsh/.zshrc ~/.zshrc
+
+# Change shell to zsh and launch it
+chsh -s /bin/zsh
+if [ $SHELL != "/bin/zsh" ]; then
+    echo "Switching to zsh"
+    zsh
+fi
+
