f850497f49f9987375d29cd3cdeb8a0de18c8253
Daniel Arndt <dan@arndt.ca>
2015-11-08 09:33:51 -0500

    Begin work on install script in python
    - re-create ask function

---
+++ install.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+
+SCRIPT_DIR = os.getcwd()
+
+def ask(question, default=None):
+    yes_answers = {'Y', 'y', 'yes'}
+    no_answers = {'N', 'n', 'no'}
+    if default is None:
+        prompt = "y/n"
+    elif default:
+        prompt = "Y/n"
+    elif not default:
+        prompt = "y/N"
+
+    while True:
+        user_input = raw_input(question + ' [{}]: '.format(prompt))
+        if user_input in yes_answers:
+            return True
+        if user_input in no_answers:
+            return False
+        if not user_input and default != None:
+            return default
+        print("Did not recognize input: {}".format(user_input))
+
+
