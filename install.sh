6dd0643d6a1d8e34ebea9d52bdec3392571acd24
Daniel Arndt <dan@arndt.ca>
2018-03-10 13:13:52 -0400

    Checkout vim config
    - Checkout vim-config
    - Use ansible-toolkit to run a single role

--- install.sh
+++ install.sh
@@ -6,4 +6,13 @@ if ! hash ansible-playbook > /dev/null; then
 	sudo pip install ansible || { echo "Error: \`pip install ansible\`" && exit 1; }
 fi
 
-ansible-playbook -i "localhost," -c local ansible/master.yml 
+role=$1
+
+if [[ -n "${role}" ]]; then
+	if ! hash ansible-role > /dev/null; then
+		sudo pip install ansible-toolkit
+	fi
+	(cd ansible && ansible-role -i "localhost," -c local $role)
+else
+	ansible-playbook -i "localhost," -c local ansible/master.yml 
+fi
